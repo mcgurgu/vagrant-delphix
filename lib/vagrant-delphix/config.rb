65280922ae2e841fd85171cf8c9435d47ec7bd65
Michael Kuehl <michael@ratchet.cc>
2016-01-15 10:18:11 +0100

    basic workflow & configuration

--- lib/vagrant-delphix/config.rb
+++ lib/vagrant-delphix/config.rb
@@ -1,34 +1,76 @@
 module VagrantPlugins
   module Delphix
     class Config < Vagrant.plugin('2', :config)
+      
       attr_accessor :enabled
-      attr_accessor :manage_environment
+      
       attr_accessor :engine_url
-      attr_accessor :engine_user
-      attr_accessor :engine_password
+      attr_accessor :user
+      attr_accessor :password
       
       def initialize
-        @enabled            = UNSET_VALUE
-        @manage_environment = UNSET_VALUE
-        @engine_url         = UNSET_VALUE
-        @engine_user        = UNSET_VALUE
-        @engine_password    = UNSET_VALUE
+        @enabled    = UNSET_VALUE
+        @engine_url = UNSET_VALUE
+        @user       = UNSET_VALUE
+        @password   = UNSET_VALUE
       end
 
       def finalize!
-        @enabled            = false if @enabled == UNSET_VALUE
-        @manage_environment = false if @manage_environment == UNSET_VALUE
-        @engine_url         = false if @engine_url == UNSET_VALUE
-        @engine_user        = false if @engine_user == UNSET_VALUE
-        @engine_password    = false if @engine_password == UNSET_VALUE
+        @enabled    = false if @enabled == UNSET_VALUE
+        @engine_url = nil if @engine_url == UNSET_VALUE
+        @user       = nil if @user == UNSET_VALUE
+        @password   = nil if @password == UNSET_VALUE
       end
 
       def validate(machine)
-        puts "+++ VALIDATE +++"
-        puts "--- #{@enabled}, url=#{@engine_url}"
-        errors = []
 
-        #errors << validate_bool('hostmanager.enabled', @enabled)
+        errors = []
+        errors << validate_bool('delphix.enabled', @enabled)
+        
+        if @enabled
+          # check if all values are present
+          if machine.config.delphix.engine_url.nil?
+            errors << I18n.t('vagrant_delphix.config.not_defined', {
+              :config_key => 'delphix.engine_url',
+            })
+          else
+            if !machine.config.delphix.engine_url.nil? &&
+               !machine.config.delphix.engine_url.kind_of?(String)
+              errors << I18n.t('vagrant_delphix.config.not_a_string', {
+                :config_key => 'delphix.engine_url',
+                :is_class   => engine_url.class.to_s,
+              })
+            end
+          end
+          
+          if machine.config.delphix.user.nil?
+            errors << I18n.t('vagrant_delphix.config.not_defined', {
+              :config_key => 'delphix.user',
+            })
+          else
+            if !machine.config.delphix.user.nil? &&
+               !machine.config.delphix.user.kind_of?(String)
+              errors << I18n.t('vagrant_delphix.config.not_a_string', {
+                :config_key => 'delphix.user',
+                :is_class   => user.class.to_s,
+              })
+            end
+          end
+          
+          if machine.config.delphix.password.nil?
+            errors << I18n.t('vagrant_delphix.config.not_defined', {
+              :config_key => 'delphix.password',
+            })
+          else
+            if !machine.config.delphix.password.kind_of?(String)
+              errors << I18n.t('vagrant_delphix.config.not_a_string', {
+                :config_key => 'delphix.password',
+                :is_class   => password.class.to_s,
+              })
+            end
+          end
+                  
+        end
         
         errors.compact!
         { "Delphix configuration" => errors }
--- lib/vagrant-delphix/provisioner.rb
+++ lib/vagrant-delphix/provisioner.rb
@@ -1,24 +1,22 @@
-#require 'vagrant-hostmanager/hosts_file/updater'
 
 module VagrantPlugins
   module Delphix
+
     class Provisioner < Vagrant.plugin('2', :provisioner)
 
       def initialize(machine, config)
-        puts "+++ PROVISION.init +++"
         super(machine, config)
-        global_env = machine.env
-        @config = Util.get_config(global_env)
-        #@updater = HostsFile::Updater.new(global_env, machine.provider_name)
+        
+        @config = machine.env.vagrantfile.config.delphix
+    
       end
 
       def provision
         puts "+++ PROVISION +++"
-        #@updater.update_guest(@machine)
-        #if @config.hostmanager.manage_host?
-        #  @updater.update_host
-        #end
+        puts "--- #{@config.engine_url}"
       end
-    end
+      
+    end # end Class
+    
   end
 end
